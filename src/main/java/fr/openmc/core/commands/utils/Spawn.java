db03a5d6346094455b87ed6ae6f5a1776aa299ab
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-05-22 19:41:51 +0200

    Change le temp d'absobrissement qd /spawn (#402)
    * change: https://github.com/ServerOpenMC/PluginV2/issues/329
    
    * change: https://github.com/ServerOpenMC/PluginV2/issues/329

--- src/main/java/fr/openmc/core/commands/utils/Spawn.java
+++ src/main/java/fr/openmc/core/commands/utils/Spawn.java
@@ -1,18 +1,19 @@
 package fr.openmc.core.commands.utils;
 
 import fr.openmc.core.OMCPlugin;
+import fr.openmc.core.utils.messages.MessageType;
+import fr.openmc.core.utils.messages.MessagesManager;
+import fr.openmc.core.utils.messages.MessagesManager.Message;
+import fr.openmc.core.utils.messages.Prefix;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import fr.openmc.core.utils.messages.MessageType;
-import fr.openmc.core.utils.messages.MessagesManager;
-import fr.openmc.core.utils.messages.Prefix;
-import fr.openmc.core.utils.messages.MessagesManager.Message;
 import org.bukkit.scheduler.BukkitRunnable;
-import revxrsal.commands.annotation.*;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Default;
+import revxrsal.commands.annotation.Description;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 public class Spawn {
@@ -37,7 +38,7 @@ public class Spawn {
                         }
                     }.runTaskLater(OMCPlugin.getInstance(), 10);
                 }
-            }.runTaskLater(OMCPlugin.getInstance(), 10);
+            }.runTaskLater(OMCPlugin.getInstance(), 15);
         } else {
             if(!(sender instanceof Player) || ((Player) sender).hasPermission("omc.admin.commands.spawn.others")) {
                 new BukkitRunnable() {
@@ -52,7 +53,7 @@ public class Spawn {
                                 MessagesManager.sendMessage(target, Component.text("§aVous avez été envoyé au spawn par §e" + (sender instanceof Player player ? player.getName() : "Console") + "§a"), Prefix.OPENMC, MessageType.WARNING, true);                            }
                         }.runTaskLater(OMCPlugin.getInstance(), 10);
                     }
-                }.runTaskLater(OMCPlugin.getInstance(), 10);
+                }.runTaskLater(OMCPlugin.getInstance(), 15);
             } else {
                 MessagesManager.sendMessage(sender, Message.NOPERMISSION.getMessage(), Prefix.OPENMC, MessageType.ERROR, true);
             }
