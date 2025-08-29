eea74e2850477d1c37ba7d3566160efec77f5f21
iambibi_ <89582596+iambibi@users.noreply.github.com>
2025-08-29 21:29:53 +0200

    hotfix: missing import

--- src/main/java/fr/openmc/core/listeners/InteractListener.java
+++ src/main/java/fr/openmc/core/listeners/InteractListener.java
@@ -5,6 +5,7 @@ import fr.openmc.core.items.usable.CustomUsableItemRegistry;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
